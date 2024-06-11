c398f597d241c397fecdd1c5895800c1cbeb236c
adyelson <adyelson.loureiro@gmail.com>
2024-06-11 13:15:22 -0300

    FORMULA ACEITAVEL - COM ARREDONDAMENTO CH

--- br/ferramentas/autoescala/globalvar.js
+++ br/ferramentas/autoescala/globalvar.js
@@ -6,18 +6,20 @@
 //
 // Adyelson Felix Loureiro
 //
-// f3lixtech@f3lixtech.com 
+// f3lixtech@f3lixtech.com
 // ------------------------------------------------------
 // ------------------------------------------------------
 // ------------------------------------------------------
 // ------------------------------------------------------
 let startDate = new Date();
-if(startDate.getMonth()<10){
-
-	document.querySelector('#qtdDiasMes').value =`${(startDate.getYear()+1900)}-0${startDate.getMonth()+1}`;
-}else{
-
-	document.querySelector('#qtdDiasMes').value =`${(startDate.getYear()+1900)}-${startDate.getMonth()+1}`;
+if (startDate.getMonth() < 10) {
+  document.querySelector("#qtdDiasMes").value = `${
+    startDate.getYear() + 1900
+  }-0${startDate.getMonth() + 1}`;
+} else {
+  document.querySelector("#qtdDiasMes").value = `${
+    startDate.getYear() + 1900
+  }-${startDate.getMonth() + 1}`;
 }
 
 let indexLane = 0;
@@ -31,47 +33,77 @@ let headerShiftNormal = [];
 let definedShifts = [];
 let holidays = [];
 let shiftList = new Vue({
-	el:'#shiftList', 
-	data:{
-		workers: workers, //array
-		workerList: workerList,//obj objt
-		amountDays: 0,
-		headerShift: headerShiftNormal,
-		definedShifts: definedShifts
-	},
-	methods:{
-		varToString: function(a){
-			return (Object.keys(a)[0]);
-		}
-			
-	}
+  el: "#shiftList",
+  data: {
+    workers: workers, //array
+    workerList: workerList, //obj objt
+    amountDays: 0,
+    headerShift: headerShiftNormal,
+    definedShifts: definedShifts,
+  },
+  methods: {
+    varToString: function (a) {
+      return Object.keys(a)[0];
+    },
+    roundToTwo(num) {
+      return parseFloat(num.toFixed(2));
+    },
+  },
 });
-let monthShift= [];
+let monthShift = [];
 let workIdshiftList = new Vue({
-	el:'#workIdshiftList', 
-	data:{
-		shifts: convertedShift,
-		header: headerConvertedShift,
-		amountDays: 0		
-		
-	}
+  el: "#workIdshiftList",
+  data: {
+    shifts: convertedShift,
+    header: headerConvertedShift,
+    amountDays: 0,
+  },
 });
 let priorityList = new Vue({
-	el:'#prioritylevel',
-	data:{
-		optionsa: ['Tipo do turno','Tipo do dia','Carga Horária','Total de dias',""],
-		optionsb: ['Tipo do dia','Carga Horária','Total de dias','Tipo do turno',""],
-		optionsc: ['Carga Horária','Total de dias','Tipo do turno','Tipo do dia',""],
-		optionsd: ['Total de dias','Tipo do turno','Tipo do dia','Carga Horária',""],
-		//optionse: ['Fim de semana','Total de dias','Tipo do turno','Tipo do dia','Carga Horária',""],
-	}
-})
+  el: "#prioritylevel",
+  data: {
+    optionsa: [
+      "Tipo do turno",
+      "Tipo do dia",
+      "Carga Horária",
+      "Total de dias",
+      "",
+    ],
+    optionsb: [
+      "Tipo do dia",
+      "Carga Horária",
+      "Total de dias",
+      "Tipo do turno",
+      "",
+    ],
+    optionsc: [
+      "Carga Horária",
+      "Total de dias",
+      "Tipo do turno",
+      "Tipo do dia",
+      "",
+    ],
+    optionsd: [
+      "Total de dias",
+      "Tipo do turno",
+      "Tipo do dia",
+      "Carga Horária",
+      "",
+    ],
+    //optionse: ['Fim de semana','Total de dias','Tipo do turno','Tipo do dia','Carga Horária',""],
+  },
+});
 
 let seeListOfWorkers = 0;
 let seeShiftmonth = 0;
-let specialSituation = [{type: 'dayoff', legend: '', startData: '', endData: '', free: false, plus: 0},]
+let specialSituation = [
+  {
+    type: "dayoff",
+    legend: "",
+    startData: "",
+    endData: "",
+    free: false,
+    plus: 0,
+  },
+];
 let shiftModelWeek = [];
-
-
-
-
