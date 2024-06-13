06acb8f7dbd236576f7935d71a0db7dc47ab7e95
adyelson <adyelson.loureiro@gmail.com>
2024-06-13 12:47:49 -0300

    funciona

--- br/portifolio/js/projetos.js
+++ br/portifolio/js/projetos.js
@@ -228,7 +228,245 @@ let projetos = [
     qtd_img: 1,
   },
 ];
+let sobremim = [
+  {
+    titulo: "Criador da F3lixTech",
+    textop1:
+      "Felix é o criador da F3lixTech,  que oferece soluções tecnológicas personalizadas por meio de metodologias ágeis e consultorias especializadas.",
+  },
+  {
+    titulo: "História com Tecnologia",
+    textop1:
+      "Há " +
+      (anoatual - 2005) +
+      " anos, Felix teve seu primeiro contato com a tecnologia, quando teve seu primeiro computador. Desde então, ele mostrou uma curiosidade natural em aprender sobre o funcionamento interno do sistema operacional e seus softwares.",
+    textop2:
+      "Na época, ele já corrigia erros e substituía dll no Windows 98. Pouco depois, aprendeu a editar arquivos em linguagem Lua para personalizar um servidor de Tibia. Isso despertou um interesse ainda maior em desenvolver suas habilidades em programação. Anos depois, ele se aventurou a criar servidores de Ragnarok e Minecraft, demonstrando uma compreensão cada vez maior em relação aos conceitos da tecnologia;",
+    textop3:
+      "Em 2009, ele concluiu um curso técnico em informática, o que o permitiu expandir ainda mais seu conhecimento e habilidades. Atualmente, Felix está se formando em Tecnologia da Informação, além de pós-graduação em Engenharia de Software, mostrando seu comprometimento em continuar aprimorando seus conhecimentos e habilidades para fornecer soluções tecnológicas cada vez mais avançadas e eficazes.",
+    textop4:
+      "Membro ativo da comunidade Sem Codar desde 2021, compartilha seus conhecimentos e experiências com outros entusiastas da tecnologia. Ele está juntando todas as suas habilidades, tanto em programação tradicional quanto em no-code, para extrair o melhor que o mundo da tecnologia pode oferecer.",
+  },
+  {
+    titulo: "Cursos",
+    texto: "",
+    lista: [
+      "Pós em Engenharia de Software - MBA",
+      "Bacharelado em Tecnologia da Informação",
+      "Técnico em Informática",
+      "Bacharelado em Ciências Aeronáuticas",
+      "Técnico em Controle de Tráfego Aéreo",
+    ],
+  },
+  {
+    titulo: "Certificados",
+    texto: "",
+    lista: [
+      "Sem Codar",
+      "GIT/GITHUB",
+      "Javascript",
+      "Docker",
+      "Sass",
+      "HTML",
+      "REACT",
+    ],
+  },
+  {
+    titulo: "Treinamentos realizados",
+    texto: "",
+    lista: [
+      "POO/Java - Curso em Vídeo",
+      "Spring/React - DevSuperior",
+      "Hackaton Gemini - Iteris",
+    ],
+  },
+  {
+    titulo: "Outros conhecimentos/habilidades",
+    texto: "",
+    lista: [
+      "Bubble",
+      "Appgyver",
+      "Vue",
+      "Bootstrap",
+      "CSS",
+      "Google Sheet/Excel",
+      "Python",
+      "Criação de plugins - Bubble",
+      "SQL",
+      "MySQL",
+      "Shellscript/Linux",
+      "Integração API",
+    ],
+  },
+];
 
+let politicasdeprivacidade = {
+  "Política de Privacidade para F3lixTech":
+    "A privacidade dos visitantes do site F3lixTech é de grande importância e esta política de privacidade descreve como as informações pessoais serão coletadas, usadas, compartilhadas e protegidas.",
+
+  "Coleta de Informações Pessoais":
+    "Informações pessoais dos visitantes do site F3lixTech não serão coletadas, a menos que sejam fornecidas voluntariamente. As informações pessoais podem incluir nome, endereço de e-mail e número de telefone, e serão utilizadas apenas para responder a perguntas ou solicitações de contato.",
+
+  "Uso de Informações Pessoais":
+    "As informações pessoais coletadas serão utilizadas exclusivamente para responder a perguntas ou solicitações de contato. Não haverá compartilhamento dessas informações com terceiros, a menos que exigido por lei.",
+
+  "Cookies e Tecnologias de Rastreamento":
+    "O site F3lixTech pode utilizar cookies e outras tecnologias de rastreamento para melhorar a experiência do usuário e coletar informações sobre como os visitantes utilizam o site. As informações podem incluir endereço IP, tipo de navegador, provedor de serviços de internet (ISP), páginas visitadas e tempo gasto no site. As informações coletadas serão utilizadas apenas para melhorar a experiência do usuário e analisar o tráfego do site.",
+
+  "Links para Outros Sites":
+    "O site F3lixTech pode conter links para outros sites que não são operados por ele. Não há controle sobre o conteúdo e as práticas de privacidade desses sites, e não há responsabilidade pela política de privacidade deles.",
+
+  "Segurança das Informações Pessoais":
+    "Medidas razoáveis serão tomadas para proteger as informações pessoais coletadas contra acesso não autorizado, uso, divulgação, alteração ou destruição. No entanto, devido à natureza da internet, não é possível garantir a completa segurança das informações pessoais contra violações de segurança.",
+
+  "Alterações na Política de Privacidade":
+    "A política de privacidade pode ser atualizada de tempos em tempos e a versão mais recente estará sempre disponível no site F3lixTech.",
+
+  Contato:
+    "Em caso de dúvidas ou preocupações sobre a política de privacidade, é possível entrar em contato por meio do site F3lixTech.",
+};
+
+let termosdeuso = {
+  "Termos de Uso para o site F3lixTech":
+    "Estes termos de uso descrevem os direitos e responsabilidades do usuário ao acessar e usar o site F3lixTech. Ao acessar e utilizar o site, o usuário concorda com estes termos de uso.",
+
+  "Conteúdo do Site":
+    "Todo o conteúdo do site F3lixTech, incluindo texto, imagens, vídeos, áudios e outros materiais, é protegido por direitos autorais e é de propriedade exclusiva da F3lixTech, a menos que indicado de outra forma. O usuário não pode copiar, reproduzir, distribuir, transmitir, exibir, vender, licenciar ou explorar comercialmente qualquer conteúdo do site sem a permissão prévia por escrito da F3lixTech.",
+
+  "Links para Outros Sites":
+    "O site F3lixTech pode conter links para outros sites que não são operados pela F3lixTech. A F3lixTech não é responsável pelo conteúdo e pelas práticas de privacidade desses sites e não endossa nem assume qualquer responsabilidade por qualquer conteúdo, publicidade, produtos ou outros materiais nesses sites. O usuário é responsável por avaliar e assumir os riscos associados ao acesso e uso desses sites.",
+
+  "Limitação de Responsabilidade":
+    "O site F3lixTech é fornecido como está e a F3lixTech não oferece garantias de qualquer tipo, expressas ou implícitas, incluindo, mas não se limitando a, garantias de adequação a uma finalidade específica, não violação ou operação sem erros ou interrupções. Em nenhuma circunstância a F3lixTech será responsável por quaisquer danos diretos, indiretos, punitivos, incidentais, especiais ou consequentes resultantes ou relacionados ao uso do site ou da incapacidade de usá-lo.",
+
+  "Alterações nos Termos de Uso":
+    "A F3lixTech se reserva o direito de atualizar ou modificar estes termos de uso a qualquer momento e sem aviso prévio. O uso continuado do site após a publicação de quaisquer alterações aos termos de uso constitui aceitação dessas alterações.",
+
+  "Lei Aplicável":
+    "Estes termos de uso serão regidos e interpretados de acordo com as leis do país onde a F3lixTech está sediada, sem considerar conflitos de disposições legais. O usuário concorda em submeter-se à jurisdição exclusiva dos tribunais desse país em relação a qualquer disputa decorrente ou relacionada ao uso do site F3lixTech.",
+
+  Contato:
+    "Se o usuário tiver alguma dúvida ou preocupação sobre estes termos de uso, entre em contato com a F3lixTech através do site.",
+};
+
+let pagedatatextos = {
+  tituloprincipal: {
+    en: "Turn your ideas into customized solutions",
+    br: "Transforme suas ideias em soluções personalizadas",
+  },
+  subtituloprincipal: {
+    en: "Smart and innovative solutions with agile methods and specialized consulting.",
+    br: "Soluções inteligentes e inovadoras com métodos ágeis e consultorias especializadas.",
+  },
+  entrarcontato: {
+    en: "Contact us",
+    br: "Entrar em contato",
+  },
+  saibamais: {
+    en: "More",
+    br: "Saiba mais",
+  },
+  call1titulo: {
+    en: "More",
+    br: "Otimização",
+  },
+  call1texto: {
+    en: "More",
+    br: "Soluções para poupar tempo e evitar gastos em processos repetitivos, tudo na palma da sua mão.",
+  },
+  call2titulo: {
+    en: "More",
+    br: "Multiplataforma",
+  },
+  call2texto: {
+    en: "More",
+    br: "Acesse sua aplicação sempre que quiser, de qualquer dispositivo.",
+  },
+  introsolucao: {
+    en: "More",
+    br: "Processos repetitivos e complicados?",
+  },
+  solucaotitulo: {
+    en: "More",
+    br: "Soluções",
+  },
+  solucao1titulo: {
+    en: "More",
+    br: "Acessibilidade",
+  },
+  solucao1texto: {
+    en: "More",
+    br: "Não sofra mais com limitação de acesso, algo comum em sistemas antigos.",
+  },
+  solucao2titulo: {
+    en: "More",
+    br: "Integrações",
+  },
+  solucao2texto: {
+    en: "More",
+    br: "Sua aplicação terá acesso as informações necessárias concentradas em um só lugar.",
+  },
+  solucao3titulo: {
+    en: "More",
+    br: "Inovação",
+  },
+  solucao3texto: {
+    en: "More",
+    br: "Escolha exatamente como sua aplicação irá te ajudar.",
+  },
+  solucao4titulo: {
+    en: "More",
+    br: "Redução de Custo",
+  },
+  solucao4texto: {
+    en: "More",
+    br: "Evite custos desnecessários com operações manuais que erram e causam prejuízos.",
+  },
+  solucao5titulo: {
+    en: "More",
+    br: "Velocidade",
+  },
+  solucao5texto: {
+    en: "More",
+    br: "Com processos otimizados e automatizados, as informações sempre estarão atualizadas.",
+  },
+  solucao6titulo: {
+    en: "More",
+    br: "Disponibilidade",
+  },
+  solucao6texto: {
+    en: "More",
+    br: "Servidores que oferecem maior disponibilidade para acesso que qualquer outro no mundo.",
+  },
+  projetolink: {
+    en: "More",
+    br: "Projetos",
+  },
+  tecnologiaslink: {
+    en: "More",
+    br: "Tecnologias",
+  },
+  melhorescolhalink: {
+    en: "More",
+    br: "Melhor escolha",
+  },
+  projetostiutlo: {
+    en: "More",
+    br: "Projetos",
+  },
+  projetossubtitulo: {
+    en: "More",
+    br: "skills code e no-code",
+  },
+  projetosintroducao: {
+    en: "More",
+    br: "Veja abaixo alguns projetos desenvolvidos com bubble e outros que são ótimos exemplos das habilidades de codificar que existem na F3lixtech.",
+  },
+  filtrar: {
+    en: "Filter",
+    br: "Filtrar",
+  },
+};
 function nome_projeto(titulo) {
   return titulo.toLowerCase().replaceAll(" ", "_");
 }
@@ -240,6 +478,19 @@ function listar_imagens(nome, qtd_img) {
   return imagens;
 }
 
+tags = [...new Set(tags)].sort();
+
+const projetoitem = document.querySelectorAll(".projetoitem");
+let lista_filtro_projeto = [];
+const tagsSpan = document.querySelector(".modal-tags");
+
+const imgElem = document.querySelector(".modal-img");
+
+const imgDivElem = document.querySelector(".modal-img-div");
+imgDivElem.innerHTML = "";
+
+let filtroProjetos = document.querySelector(".filtrar-projetos");
+let listaDeFiltros = document.querySelector(".lista-de-filtros");
 projetos.forEach((projeto) => {
   projeto.nome = nome_projeto(projeto.titulo);
   projeto.imagens = listar_imagens(projeto.nome, projeto.qtd_img);
@@ -248,10 +499,18 @@ projetos.forEach((projeto) => {
   });
 });
 
-tags = [...new Set(tags)].sort();
-
-const projetoitem = document.querySelectorAll(".projetoitem");
-let lista_filtro_projeto = [];
+let pagedata = new Vue({
+  el: ".pagedata",
+  data: {
+    pagedata: pagedatatextos,
+    pagelanguage: pagelanguage,
+    projetos: projetos,
+    tags: tags,
+    politicas: politicasdeprivacidade,
+    termos: termosdeuso,
+    sobre: sobremim,
+  },
+});
 
 document.querySelectorAll(".tagbutton").forEach((el) => {
   el.addEventListener("click", () => {
@@ -287,12 +546,7 @@ document.querySelectorAll(".tagbutton").forEach((el) => {
     });
   });
 });
-const tagsSpan = document.querySelector(".modal-tags");
 
-const imgElem = document.querySelector(".modal-img");
-
-const imgDivElem = document.querySelector(".modal-img-div");
-imgDivElem.innerHTML = "";
 document.querySelectorAll(".modal-projeto-open").forEach((el) => {
   el.addEventListener("click", () => {
     let idcod = el.getAttribute("id");
@@ -352,9 +606,6 @@ document.querySelectorAll(".modal-projeto-open").forEach((el) => {
     document.querySelector(".modal-testar").setAttribute("href", projeto.link);
   });
 });
-
-let filtroProjetos = document.querySelector(".filtrar-projetos");
-let listaDeFiltros = document.querySelector(".lista-de-filtros");
 
 filtroProjetos.addEventListener("click", function (ev) {
   if (filtroProjetos.classList.contains("color-main")) {
